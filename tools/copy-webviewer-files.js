4ef6a7d07678b76fe3a9810c46c97fea3c9047ae
phenethylamine <dustin.riley@me.com>
2020-04-17 16:03:49 -0700

    pdfjs express electron sample

---
+++ tools/copy-webviewer-files.js
@@ -0,0 +1,16 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const copyFiles = async () => {
+  const sourcePath = path.resolve('./node_modules/@pdfjs-express/webviewer/public');
+  const destPath = path.resolve('./public/lib/');
+
+  try {
+    await fs.copy(sourcePath, destPath);
+    console.log('WebViewer files copied over successfully');
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+copyFiles(); 
